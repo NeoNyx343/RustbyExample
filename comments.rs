368378feb75916019c0017aeb7581b1cb2864b79
NeoNyx343 <lifelovesfrnds@gmail.com>
2022-02-18 05:53:35 +0530

    about comments

---
+++ comments.rs
@@ -0,0 +1,19 @@
+// This is a line comment
+/* This is a type
+ * of block comment
+ * Typically used for
+ * multiple lines
+ * */
+
+// Multi line comments can also be 
+// used to debug the program 
+// by commenting out certain blocks of
+// code
+
+fn main() {
+    // println!("Hello, world!");
+    println!("Hello, world!");
+    // let x = 5 + /* 90 + */ 5;
+    let x = 5 + 90 + 5;
+    println!("Is 'x' 10 or 100? x = {}", x);
+}
